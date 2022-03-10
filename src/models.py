40f63ed2feaeabc3ae48f41e2aadcae321d5479d
ron24g <ron24g@yahoo.com>
2022-03-10 02:01:57 +0000

    first copy

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,21 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    posts = relationship("Post", back_populates="author")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    created = Column(DateTime, nullable=False)
+    updated = Column(DateTime, nullable=True)
+    content = Column(String, nullable=True)
+    author = Column(Integer, ForeignKey('users.id'))
 
-    def to_dict(self):
-        return {}
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
